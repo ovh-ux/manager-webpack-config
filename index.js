6708feff757fb14d04144cf78819943640e857e0
jleveugle <joffrey.leveugle@corp.ovh.com>
2018-09-19 14:18:40 +0200

    build: update production condition

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const common = require('./webpack.common');
 const devConfig = require('./webpack.dev');
 const prodConfig = require('./webpack.prod');
 
-module.exports = (opts) => {
+module.exports = (opts, env = {}) => {
   const commonConfig = common(opts);
-  const config = merge(commonConfig, process.env.NODE_ENV !== 'production' ? devConfig : prodConfig);
+  const config = merge(commonConfig, env.production ? prodConfig : devConfig);
 
   return {
     commonConfig,
