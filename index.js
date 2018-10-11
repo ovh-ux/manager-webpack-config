af086431612e2d9d0b391cf7b12daababe515323
Florian Renaut <florian.renaut@corp.ovh.com>
2018-10-11 14:07:27 +0200

    fix(server): externalize server configuration
    Conflicts:
    	webpack.dev.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const merge = require('webpack-merge');
+const devServer = require('manager-webpack-dev-server');
 const common = require('./webpack.common');
-const dev = require('./webpack.dev');
 const prodConfig = require('./webpack.prod');
 
 module.exports = (opts, env = {}) => {
   const commonConfig = common(opts);
-  const devConfig = dev(env);
+  const devConfig = devServer.config(env);
   const config = merge(commonConfig, env.production ? prodConfig : devConfig);
 
   return {
